8fb970f83f19e976b3807368e3405152fef6b6f9
cs2student <katie@acornonsite.com>
2016-11-22 23:53:32 -0800

    at a random spot, not anywhere done yet

--- hash_table.c
+++ hash_table.c
@@ -21,24 +21,48 @@
 
 /*** Hash function. ***/
 
-int hash(char *s)
-{
+int hash(char *s) {
+    int sum = 0;
+    int i = 0;
 
+    for(i = 0; i < sizeof s; i ++) {
+        sum = sum + s[i];
+    }
+
+    return sum % NSLOTS;
 }
 
 
 /*** Linked list utilities. ***/
 
 /* Create a single node. */
-node *create_node(char *key, int value)
-{
+node *create_node(char *key, int value) {
+    node *result = (node *)malloc(sizeof(node));
+
+    if (result == NULL) {
+        fprintf(stderr, "Fatal error: out of memory. "
+                "Terminating program.\n");
+        exit(1);
+    }
+
+    result->key = key;
+    result->value = value;
+    result->next = NULL;
+
+    return result;
 
 }
 
 
 /* Free all the nodes of a linked list. */
-void free_list(node *list)
-{
+void free_list(node *list) {
+    node *n;     /* a single node of the list */
+
+    while (list != NULL) {
+        n = list;
+        list = list->next;
+        free(n);
+    }
 
 }
 
@@ -46,16 +70,37 @@ void free_list(node *list)
 /*** Hash table utilities. ***/
 
 /* Create a new hash table. */
-hash_table *create_hash_table()
-{
+hash_table *create_hash_table() {
+    hash_table *result = (hash_table *)malloc(sizeof(hash_table));
+    node *slotstart  = (node *) malloc(sizeof(node));
+    if (result == NULL || slotstart == NULL) {
+        fprintf(stderr, "Fatal error: out of memory. "
+                "Terminating program.\n");
+        exit(1);
+    }
+
+    result->slot = &(slotstart);
+    return result;
 
 }
 
 
 /* Free a hash table. */
-void free_hash_table(hash_table *ht)
-{
-
+void free_hash_table(hash_table *ht){
+    node *list = *(ht->slot); 
+    node *n;
+    while (list != NULL) {
+        n = list;
+        list = list->next;
+        free_list(n);
+        /*
+         * 'n' now points to the first element of the list, while
+         * 'list' now points to everything but the first element.
+         * Since nothing points to 'n', it can be freed.
+         */
+        
+        /*free(n);*/
+    }
 }
 
 
@@ -63,9 +108,30 @@ void free_hash_table(hash_table *ht)
  * Look for a key in the hash table.  Return 0 if not found.
  * If it is found return the associated value.
  */
-int get_value(hash_table *ht, char *key)
-{
+int get_value(hash_table *ht, char *key) {
+    int value = hash(key);
+    node *key_list = NULL;
+    /* set list to the first node of the slot array. */
+    node *list = *(ht->slot);
+    
+    /* check every node of the array of arrays. */
+    while(list != NULL) {
+        key_list = list->value;
+        list = list->next;
+            
+        /* when correct value, make sure key is in it. */
+        if(key_list->value == value) {
+            while(key_list != NULL) {
+                if(key_list->key == key) {
+                    return value;
+                }
+                key_list = key_list->next;
+            }
+            return 0;
+        }
+    }
 
+    return 0;
 }
 
 
@@ -74,9 +140,42 @@ int get_value(hash_table *ht, char *key)
  * create a new node and set the value to 'value'.  Note that this
  * function alters the hash table that was passed to it.
  */
-void set_value(hash_table *ht, char *key, int value)
-{
+void set_value(hash_table *ht, char *key, int value){
+    node *key_list = NULL;
+    node *temp = NULL;
+    /* set list to the first node of the slot array. */
+    node *list = *(ht->slot);
+            
+    /* When key is already in hashtable. */
+    if(get_value != 0) {
+        
+        /* check every node of the array of arrays. */
+        while(list != NULL) {
+            key_list = list->value;
+            list = list->next;
+                                            
+            /* when correct value, add a key to the end of the list. */
+            if(key_list->value == value) {
+                /* find the last link. */
+                while(key_list != NULL) {
+                    key_list = key_list->next;
+                }
+            
+                /* add new node to the end */
+                temp = create_node(key, value);
+            
+                /* make the last node point to the new node */
+                key_list->next = temp;
 
+                /* make temp the new end. */
+                key_list = temp;
+            }
+        }
+
+    } else { /* when the value doesn't already exist, add a node. */
+
+
+    }
 }
 
 
